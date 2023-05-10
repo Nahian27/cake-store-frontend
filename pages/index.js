7a46804d3881385a115c6beb33bf00282245af0b
Nahian27 <alnpulok27@gmail.com>
2023-05-11 04:20:02 +0600

    ISR

--- pages/index.js
+++ pages/index.js
@@ -8,14 +8,12 @@ export default function Home({ products }) {
             <HeroBanner />
             <Wrapper>
                 {/* heading and paragaph start */}
-                <div className="text-center max-w-[800px] mx-auto my-[50px] md:my-[80px]">
+                <div className="text-center max-w-[1000px] mx-auto my-[50px] md:my-[80px]">
                     <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
-                        Best Cakes in Town
+                        "Savor Sweet Moments: Your One-Stop Online Cake Shop!"
                     </div>
                     <div className="text-md md:text-xl">
-                        A lightweight Nike ZoomX midsole is combined with
-                        increased stack heights to help provide cushioning
-                        during extended stretches of running.
+                        Delight in heavenly confections from our online cake shop. From irresistible flavors to seamless delivery, we make every celebration sweeter.
                     </div>
                 </div>
                 {/* heading and paragaph end */}
